116368a2aee6847f53c56a8b9754ed8b6d212999
root <zhangshuaishuai@cabrtech.com>
2018-04-19 14:39:57 +0800

    r

---
+++ pkpm-redis-util/src/main/java/com/pkpmdesktopcloud/redis/JDKSerializer.java
@@ -0,0 +1,36 @@
+package com.pkpmdesktopcloud.redis;
+
+import org.apache.ibatis.cache.CacheException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class JDKSerializer implements Serializer{
+
+    @Override
+    public byte[] serialize(Object object) {
+        try {
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+            objectOutputStream.writeObject(object);
+            return byteArrayOutputStream.toByteArray();
+        }
+        catch (Exception e) {
+            throw new CacheException(e);
+        }
+    }
+
+    @Override
+    public Object unserialize(byte[] bytes) {
+        if(bytes == null) return null;
+        try {
+            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+            return objectInputStream.readObject();
+        }
+        catch (Exception e) {
+            throw new CacheException(e);
+        }
+    }
+}
