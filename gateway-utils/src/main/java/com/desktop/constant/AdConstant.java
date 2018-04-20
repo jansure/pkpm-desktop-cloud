8ee27085fc83acefda90c920a1cbcc880be083c0
root <qdzhangshuai@yeah.net>
2018-04-21 01:30:49 +0800

    增加清楚对象成员方法

--- gateway-utils/src/main/java/com/desktop/constant/AdConstant.java
+++ gateway-utils/src/main/java/com/desktop/constant/AdConstant.java
@@ -4,5 +4,4 @@ public class AdConstant {
     public static final Integer UF_NORMAL_ACCOUNT = 0x0200;
     public static final Integer UF_PASSWORD_EXPIRED = 0x800000;
     public static final Integer AD_MAX_COUNT = 2000;
-    public static final Integer AD_COMPUTER_NAME_MAX_LEN = 15;
 }
