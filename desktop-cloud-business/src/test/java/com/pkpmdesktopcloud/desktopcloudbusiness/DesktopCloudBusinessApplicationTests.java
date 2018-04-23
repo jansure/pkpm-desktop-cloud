5bf1b1c88127af850f1163e783f1bdb48bd62aff
root <zhangshuaishuai@cabrtech.com>
2018-04-23 16:01:21 +0800

    add module desktop-cloud-business

---
+++ desktop-cloud-business/src/test/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pkpmdesktopcloud.desktopcloudbusiness;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesktopCloudBusinessApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
