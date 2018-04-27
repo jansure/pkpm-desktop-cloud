3baea11f0dbfe16e7cf3c7762fd2e23e390e9be1
root <zhangshuaishuai@cabrtech.com>
2018-04-27 17:02:12 +0800

    Merge branch 'dev' of codemanager.pkpm.com:root/pkpm-desktop-cloud into origin/dev
    Conflicts:
    	desktop-cloud-business/pom.xml

--- pkpm-cloud-business/src/main/java/com/cabr/pkpm/entity/subscription/SubsCription.java
+++ pkpm-cloud-business/src/main/java/com/cabr/pkpm/entity/subscription/SubsCription.java
@@ -10,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 public class SubsCription implements Serializable {
 	private Integer id;
 	private Long subsId;
+	private String areaCode;
 	private Integer userId;
 	private LocalDateTime createTime;
 	private String remark;
