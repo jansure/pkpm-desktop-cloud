923b3bee57e0b95746daafa70fc7b26445407cca
root <qdzhangshuai@yeah.net>
2018-05-01 23:33:30 +0800

    修改代码

---
+++ desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/service/SysConfigService.java
@@ -0,0 +1,7 @@
+package com.pkpmdesktopcloud.desktopcloudbusiness.service;
+
+import com.pkpmdesktopcloud.desktopcloudbusiness.domain.SysConfig;
+
+public interface SysConfigService {
+    SysConfig getSysConfigByKey(String key);
+}
