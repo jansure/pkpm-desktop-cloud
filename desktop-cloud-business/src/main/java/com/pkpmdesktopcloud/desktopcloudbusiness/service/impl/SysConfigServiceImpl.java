5b75a568f2eb3d70539ead54f025e70329c58d40
root <qdzhangshuai@yeah.net>
2018-05-01 23:35:34 +0800

    修改代码

--- desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/service/impl/SysConfigServiceImpl.java
+++ desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/service/impl/SysConfigServiceImpl.java
@@ -5,12 +5,14 @@ import com.google.common.base.Preconditions;
 import com.pkpmdesktopcloud.desktopcloudbusiness.dao.SysConfigDAO;
 import com.pkpmdesktopcloud.desktopcloudbusiness.domain.SysConfig;
 import com.pkpmdesktopcloud.desktopcloudbusiness.service.SysConfigService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
 @Service
+@Slf4j
 public class SysConfigServiceImpl implements SysConfigService {
     @Resource
     private SysConfigDAO sysConfigDAO;
