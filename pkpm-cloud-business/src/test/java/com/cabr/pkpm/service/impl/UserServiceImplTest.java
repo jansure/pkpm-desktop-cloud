d1487794cef503104cc0af5f7da0756a8c89ce57
root <zhangshuaishuai@cabrtech.com>
2018-05-02 16:04:50 +0800

    Merge branch 'dev' of codemanager.pkpm.com:root/pkpm-desktop-cloud into origin/dev
    Conflicts:
    	pkpm-cloud-business/src/main/java/com/cabr/pkpm/service/impl/SubscriptionServiceImpl.java
    	pkpm-cloud-business/src/main/java/com/cabr/pkpm/service/impl/UserServiceImpl.java

--- pkpm-cloud-business/src/test/java/com/cabr/pkpm/service/impl/UserServiceImplTest.java
+++ pkpm-cloud-business/src/test/java/com/cabr/pkpm/service/impl/UserServiceImplTest.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+
 import static org.junit.Assert.*;
 
 /**
@@ -21,8 +23,8 @@ import static org.junit.Assert.*;
 @SpringBootTest
 @RunWith(SpringRunner.class)
 public class UserServiceImplTest {
-    @Autowired
-    UserMapper userMapper;
+    @Resource
+    private UserMapper userMapper;
 
     @Test
     public void changeUserPassword() throws Exception {
