9bd6d2e17549325259e895db4f5fcda5a40f35a3
root <qdzhangshuai@yeah.net>
2018-05-06 16:36:36 +0800

    修改代码

--- message-server/src/main/java/com/messageserver/messageserver/service/MessageSender.java
+++ message-server/src/main/java/com/messageserver/messageserver/service/MessageSender.java
@@ -1,5 +1,12 @@
 package com.messageserver.messageserver.service;
 
+
+/**
+ * EmailMessageSenderImpl
+ *
+ * author jansure.zhang
+ * @since 5/5/2018
+ */
 public interface MessageSender {
     Boolean sendMessage(Message message);
 }
