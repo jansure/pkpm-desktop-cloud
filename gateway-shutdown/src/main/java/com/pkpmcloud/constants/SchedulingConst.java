8726fff16142b181cd49905826cbfe4ce8c53fd5
=xuhe <=evanxuhe@163.com>
2018-05-09 16:07:12 +0800

    gitignore

---
+++ gateway-shutdown/src/main/java/com/pkpmcloud/constants/SchedulingConst.java
@@ -0,0 +1,11 @@
+package com.pkpmcloud.constants;
+
+/**
+ * @author xuhe
+ * @description
+ * @date 2018/5/9
+ */
+public class SchedulingConst {
+
+    public static final long TIME_INTERVAL=5000;
+}
