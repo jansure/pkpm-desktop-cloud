c94beef43356b253daf3a5205bcdcbfca52357c6
wangxiulong <wangxiulongd@163.com>
2018-05-10 10:02:33 +0800

    修改

--- file-server/src/main/java/org/cleverframe/fastdfs/FastdfsApplication.java
+++ file-server/src/main/java/org/cleverframe/fastdfs/FastdfsApplication.java
@@ -2,8 +2,9 @@ package org.cleverframe.fastdfs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class FastdfsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FastdfsApplication.class, args);
