4d0d90dc03e9b9e323e2b33f67ff38889bee9ea5
=xuhe <=evanxuhe@163.com>
2018-05-14 11:15:04 +0800

    统一为desktop-cloud模块 计算机名：根据非失败订单自增

---
+++ desktop-cloud-business/src/test/java/com/pkpmdesktopcloud/desktopcloudbusiness/dao/mapper/PkpmCloudSubscriptionMapperTest.java
@@ -0,0 +1,25 @@
+package com.pkpmdesktopcloud.desktopcloudbusiness.dao.mapper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author xuhe
+ * @description
+ * @date 2018/5/14
+ */
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class PkpmCloudSubscriptionMapperTest {
+
+
+    @Test
+    public void getNotInvalidSubscriptionList() throws Exception {
+    }
+
+}
