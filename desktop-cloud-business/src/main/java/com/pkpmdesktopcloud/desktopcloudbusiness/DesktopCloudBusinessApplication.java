64b0bdc68451291d863ac24a8f4a064f5c97fa95
root <zhangshuaishuai@cabrtech.com>
2018-05-17 15:59:49 +0800

    Merge branch 'dev' of codemanager.pkpm.com:root/pkpm-desktop-cloud into origin/dev
    Conflicts:
    	desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/controller/DocumentController.java

--- desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplication.java
+++ desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplication.java
@@ -1,11 +1,21 @@
 package com.pkpmdesktopcloud.desktopcloudbusiness;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
-@ComponentScan(basePackages={"com.pkpmdesktopcloud.desktopcloudbusiness.dao.mapper","com.desktop.utils","com.messageserver.messageserver.service"})
+@EnableScheduling
+@EnableEurekaClient
+@EnableFeignClients
+@ComponentScan(basePackages={"com.desktop.utils","com.messageserver.messageserver.service"})
+@MapperScan(basePackages = "com.pkpmdesktopcloud.desktopcloudbusiness.dao", annotationClass = Mapper.class)
 public class DesktopCloudBusinessApplication {
 
 	public static void main(String[] args) {
