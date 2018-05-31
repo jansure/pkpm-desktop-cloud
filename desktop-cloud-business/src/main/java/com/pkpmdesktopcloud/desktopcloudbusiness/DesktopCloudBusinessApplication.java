758f65109454940bb8c39b1e41b1696cc8982c8e
yangpengfei <yangpengfei@cabrtech.com>
2018-05-31 11:20:26 +0800

    测试跨域

--- desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplication.java
+++ desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -20,5 +24,20 @@ public class DesktopCloudBusinessApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesktopCloudBusinessApplication.class, args);
+	}
+	
+	// 跨域配置
+	private CorsConfiguration buildConfig() {
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.addAllowedOrigin("*");
+		corsConfiguration.addAllowedHeader("*");
+		corsConfiguration.addAllowedMethod("*");
+		return corsConfiguration;
+	}
+	// 设置跨域过滤器
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", buildConfig());
+		return new CorsFilter(source);
 	}
 }
