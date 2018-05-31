b19366d06d0de1c4ee95a4c3c5f9c10a4b62f493
yangpengfei <yangpengfei@cabrtech.com>
2018-05-31 10:27:48 +0800

    consumes="application/json"

--- desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/controller/ComponentController.java
+++ desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/controller/ComponentController.java
@@ -67,7 +67,7 @@ public class ComponentController {
 	 * @return
 	 */
 	@ApiOperation("根据产品套餐类型获取产品配置组件信息")
-	@PostMapping(value = "/subComponents", produces = "application/json")
+	@PostMapping(value = "/subComponents", consumes="application/json", produces = "application/json")
 	public ResultObject getComponentByProductType(@RequestBody Map<String, Integer> map, HttpServletResponse response) {
 		// 允许跨域调用
 		response.setHeader("Access-Control-Allow-Origin", "*");
