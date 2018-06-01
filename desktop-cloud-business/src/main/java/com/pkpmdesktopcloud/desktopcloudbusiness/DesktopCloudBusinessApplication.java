5395a32a9574171c03fa46dda94f828cadf1da5d
yangpengfei <yangpengfei@cabrtech.com>
2018-06-01 14:22:08 +0800

    删除启动类中的跨域配置

--- desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplication.java
+++ desktop-cloud-business/src/main/java/com/pkpmdesktopcloud/desktopcloudbusiness/DesktopCloudBusinessApplication.java
@@ -26,18 +26,4 @@ public class DesktopCloudBusinessApplication {
 		SpringApplication.run(DesktopCloudBusinessApplication.class, args);
 	}
 	
-	// 跨域配置
-	private CorsConfiguration buildConfig() {
-		CorsConfiguration corsConfiguration = new CorsConfiguration();
-		corsConfiguration.addAllowedOrigin("*");
-		corsConfiguration.addAllowedHeader("*");
-		corsConfiguration.addAllowedMethod("*");
-		return corsConfiguration;
-	}
-	// 设置跨域过滤器
-	public CorsFilter corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", buildConfig());
-		return new CorsFilter(source);
-	}
 }
